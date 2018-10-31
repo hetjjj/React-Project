845ce7e97d8b35ac72c987a41305df1030be3dfa
hetjjj <30998335+hetjjj@users.noreply.github.com>
2018-10-31 16:10:36 -0400

    commit/update script and html template

--- src/server/helper/renderer.js
+++ src/server/helper/renderer.js
@@ -7,21 +7,20 @@ import AppRootPage from '../../views/pages/AppRootPage';
 
 export default (req) => {
 
-    const content = renderToString(
-            <StaticRouter location={req.path} context={{}}>
-                <AppRootPage>{renderRoutes(Pages)}</AppRootPage>
-            </StaticRouter >
-    );
+const content = renderToString(
+    <StaticRouter location={req.path} context={{}}>
+        <AppRootPage>{renderRoutes(Pages)}</AppRootPage>
+    </StaticRouter >
+);
 
-    return `
-        <html>
-            <head>
-            <link rel="stylesheet" href="style.css" />
-            </head>
-            <body>
-                <div id="root">${content}</div>
-                <script src="bundle.js"></script>
-            </body>
-        </html>
+return `<html>
+    <head>
+    <link rel="stylesheet" href="style.css" />
+    </head>
+    <body>
+        <div id="root">${content}</div>
+        <script src="bundle.js"></script>
+    </body>
+</html>
     `;
 }
